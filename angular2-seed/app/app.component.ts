f08a14caacada18ce58f690ab88a520f253e508e
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-08-03 14:38:55 -0600

    components

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './course.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular</h1>'
+    template: '<h1>Hello Angular</h1><courses></courses>',
+    directives: [CoursesComponent]
 })
 export class AppComponent { }
