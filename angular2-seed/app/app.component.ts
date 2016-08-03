8d0df3ab93d474870fc0ca91afe104adf64ddf68
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-08-03 15:21:01 -0600

    built component and service

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './course.component';
+import {AuthorsComponent} from './author.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Angular</h1><courses></courses>',
-    directives: [CoursesComponent]
+    template: '<h1>Hello Angular</h1><courses></courses><authors></authors>',
+    directives: [CoursesComponent, AuthorsComponent]
 })
 export class AppComponent { }
